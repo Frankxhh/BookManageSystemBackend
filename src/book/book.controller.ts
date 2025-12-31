9b57a0a777f0eb55045067bbcb310710f6c7921a
zhangxinhui <xinhui.zhang@wuitu.com>
2025-12-31 17:10:58 +0800

    feat: add upload feature;

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,6 +1,25 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto, UpdateBookDto } from './dto/book.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import path from 'path';
+import { BadRequestException } from '@nestjs/common';
+import multer from 'multer';
+import fs from 'fs';
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    try {
+      fs.mkdirSync('uploads');
+    } catch (e) {}
+
+    cb(null, 'uploads');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9) + '-' + file.originalname;
+    cb(null, uniqueSuffix);
+  },
+});
 
 @Controller('book')
 export class BookController {
@@ -29,5 +48,28 @@ export class BookController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.bookService.remove(+id);
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      dest: 'uploads', // 保存路径
+      storage: storage,
+      limits: {
+        fileSize: 1024 * 1024 * 3,
+      },
+      fileFilter(req, file, callback) {
+        const extname = path.extname(file.originalname);
+        if (['.png', '.jpg', '.gif'].includes(extname)) {
+          callback(null, true);
+        } else {
+          callback(new BadRequestException('只能上传图片'), false);
+        }
+      },
+    }),
+  )
+  upload(@UploadedFile('file') file: Express.Multer.File) {
+    console.log(file);
+    return file.path;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestApplication } from '@nestjs/core';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestApplication>(AppModule);
 
   // 全局参数验证管道
   app.useGlobalPipes(new ValidationPipe());
+
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads',
+  });
   await app.listen(process.env.PORT ?? 9000);
 }
 bootstrap();
